a6f45e5147b3f2cb99f3f32e4ad5c1ff8cd9c41c
korayizlimek <krykor.ay01@gmail.com>
2023-03-27 13:01:13 +0300

    create react app

--- src/main/java/com/example/demo/IData/ITodoData.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.IData;
-
-import com.example.demo.Entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITodoData extends JpaRepository<Todo, String> {
-}
